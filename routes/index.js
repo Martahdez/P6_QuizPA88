22a67b8b6ca9115585ec66a2489d55033615c9f1
Marta Hernandez <marta.hernandez.muela@alumnos.upm.es>
2018-05-20 13:51:20 +0200

    p6

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/author', (req, res, next) => {
 
 //GET  questions
 router.get('/quizzes/random_play', (req, res, next) => {
-    res.render('quizzes/random_play');
+    res.render('/quizzes/random_play');
 });
 
 // Autoload for routes using :quizId
