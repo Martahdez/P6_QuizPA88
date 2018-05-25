82f8517a6eb1668b55b325112cff643bc0514371
Marta Hernandez <marta.hernandez.muela@alumnos.upm.es>
2018-05-25 10:39:02 +0200

    p6

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -182,7 +182,7 @@ exports.randomplay = (req, res, next) => {
 
 
 };
- //Los pasos 3 y 4 los hago con el metodo randomcheck
+//Los pasos 3 y 4 los hago con el metodo randomcheck
 exports.randomcheck = (req,res,next)=>{
 //Este metodo se utiliza para comprobar el otro
 
@@ -192,8 +192,8 @@ exports.randomcheck = (req,res,next)=>{
     const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
     let score = req.session.randomplay.length; //Devuelvo el numero de respuestas acertadas
     if(result===1){ //Si he acertado la respuesta
-        req.session.randomplay.push(quiz.id); //La añado al array de respuestas acertadas
-         score = req.session.randomplay.length; //Actualizo la puntuacion
+        req.session.randomplay.push(quiz); //La añado al array de respuestas acertadas
+        score = req.session.randomplay.length; //Actualizo la puntuacion
     } else { //Si fallamos hay que resetear el array de respuestas acertadas
         req.session.randomplay=[];
     }
